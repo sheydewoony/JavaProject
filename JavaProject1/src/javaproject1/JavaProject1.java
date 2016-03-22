c719f0ef3dd810821f519d66f054807805ab2010
Woony <Woony@Shehan>
2016-03-23 00:06:59 +0530

    test2

--- JavaProject1/src/javaproject1/JavaProject1.java
+++ JavaProject1/src/javaproject1/JavaProject1.java
@@ -15,7 +15,8 @@ public class JavaProject1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.print("fdfdf");
+     
+       
     }
     
 }
